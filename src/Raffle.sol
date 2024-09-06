9127399d29ad2934faeb4ed12c27f0c066799521
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-09-06 15:52:28 +0300

    building enterRaffle(

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,13 +10,28 @@ pragma solidity ^0.8.16;
  */
 
 contract Raffle {
+    /** Errors */
+    error Raffle_SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // for 0.8.26+
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+        if (msg.value <= i_entranceFee) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
+
+    /** Getter functions */
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 }
