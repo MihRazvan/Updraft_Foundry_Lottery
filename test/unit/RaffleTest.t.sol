84af5ca1224353ec0e8322f5a89d0045b54cd1fc
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-09-11 15:39:35 +0300

    setup unit test

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "lib/forge-std/src/Test.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract RaffleTest {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    address public player = makeAddr("player");
+
+    function run() {
+        DeployRaffle deployRaffle = new DeployRaffle();
+        (raffle, HelperConfig) = deployRaffle.run();
+    }
+}
