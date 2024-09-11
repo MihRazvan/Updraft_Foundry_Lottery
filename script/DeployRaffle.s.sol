890662b22ce68e946a0d6644cb92f00ba68aea9d
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-09-11 15:16:24 +0300

    add HelperConfig

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Modifier: MIT
+
+pragma solidity ^0.8.16;
+
+import {Raffle} from "../src/Raffle.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
+
+contract DeployRaffle {
+    function run() public {}
+}
